95d60b4e7373f2dd130214e43015f02389dda35a
galiardi <galiardi.dev@gmail.com>
2022-08-12 08:25:05 -0400

    MongoDB connection

--- server/src/server.js
+++ server/src/server.js
@@ -1,14 +1,34 @@
 const http = require("http");
-const { loadPlanetsData } = require("./models/planets.model");
+const mongoose = require("mongoose");
 
 const app = require("./app");
 
+const { loadPlanetsData } = require("./models/planets.model");
+
 const PORT = process.env.PORT || 7500;
+
+MONGO_URL =
+  "mongodb+srv://nasa-api:QvB7FYZr9e88I75p@nasaproject.netrotv.mongodb.net/?retryWrites=true&w=majority";
 
 const server = http.createServer(app);
 
+mongoose.connection.once("open", () => {
+  console.log("MongoDB connected..");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.log(err);
+});
+
 async function startServer() {
   await loadPlanetsData();
+  await mongoose.connect(MONGO_URL, {
+    useNewUrlParser: true,
+    // useFindAndModify: false,
+    // useCreateIndex: true,
+    useUnifiedTopology: true,
+  });
+
   server.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}...`);
   });
